53d0a9a869062647310ea3303b3c6181d966c531
jader_tao <jader_tao@qq.com>
2017-11-09 23:46:33 +0800

    refact: not done yet  :>

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,15 @@
 #! /usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var query_1 = require("./query");
 var csgo = require("commander");
-var status = {
-    team: '',
-    type: ''
+var mount = function (cli, config) {
+    cli.version(config.version);
+    config.command.forEach(function (cmd) {
+        (_a = cli
+            .command(cmd.name)
+            .alias(cmd.alias)
+            .description(cmd.description)).option.apply(_a, cmd.option);
+        var _a;
+    });
 };
-var modifyType = function (type) { return status.type = type; };
-var handler = function (name) {
-    status.team = name;
-    console.log("querying " + status.type + " info of " + status.team + ", please wait");
-    query_1.classify(status.type)(name);
-};
-csgo
-    .version('0.1.0')
-    .parse(process.argv);
-csgo
-    .command('team [name]')
-    .alias('t')
-    .description('查询队伍信息')
-    .option('--content,-c <type>', 'specify query content', modifyType)
-    .action(handler);
 csgo.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,36 @@
 import { classify } from './query'
 import * as csgo from 'commander'
 
-const status = {
-  team: '',
-  type: ''
+
+
+// const handler = (name: string) => {
+//   status.team = name
+//   console.log(`querying ${status.type} info of ${status.team}, please wait`)
+//   classify(status.type)(name)
+// }
+
+
+// csgo
+//   .version('0.1.0')
+
+// csgo
+//   .command('team [name]')
+//   .alias('t')
+//   .description('查询队伍信息')
+//   .option('--content,-c <type>', 'specify query content', modifyType)
+//   .action(handler)
+
+const mount = (cli, config) => {
+  cli.version(config.version)
+
+  config.command.forEach(cmd => {
+    cli
+      .command(cmd.name)
+      .alias(cmd.alias)
+      .description(cmd.description)
+      .option(...cmd.option)
+    // TODO: add handler
+  })
 }
-
-const modifyType = (type: string) => status.type = type
-
-const handler = (name: string) => {
-  status.team = name
-  console.log(`querying ${status.type} info of ${status.team}, please wait`)
-  classify(status.type)(name)
-}
-csgo
-  .version('0.1.0')
-  .parse(process.argv)
-
-csgo
-  .command('team [name]')
-  .alias('t')
-  .description('查询队伍信息')
-  .option('--content,-c <type>', 'specify query content', modifyType)
-  .action(handler)
-
-
 
 csgo.parse(process.argv)
