d255aed070016bf312f749a17a333b9f41fe1596
jader <jader_tao@qq.com>
2017-11-10 23:53:58 +0800

    updata: new and greateeeer config file

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
-import store, { action } from './store'
 
-const modifyContent = (payload: string) => ({ type: 'content', payload })
+import { getTeamMatch, getTeamOverview, getTeamRanking } from './query'
+
+
+
 
 export const config = {
   version: '0.1.0',
@@ -8,8 +10,18 @@ export const config = {
     {
       name: 'team [name]',
       alias: 't',
-      description: '查询队伍信息',
-      option: ['--content,-c <type>', 'specify query content', modifyContent]
+      description: '查询队伍信息,每次只能查询一种',
+      option: [
+        ['--match -m', 'query recent matches according to team'],
+        ['--overview -o', 'query overview according to team'],
+        ['--ranking -r', 'query current ranking according to team']
+      ],
+      handler: (name: string, options: any) => {
+        if (options.match && !options.overview && !options.ranking) getTeamMatch(name)
+        if (!options.match && options.overview && !options.ranking) getTeamOverview(name)
+        if (!options.match && !options.overview && options.ranking) getTeamRanking(name)
+      }
     }
-  ]
+  ],
+  handler: ''
 }
