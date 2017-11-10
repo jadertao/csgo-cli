ecfe75a4f6fcbb447edbf719cb3d1a956e524a33
jader <jader_tao@qq.com>
2017-11-10 23:53:33 +0800

    updata: add and compose fn

--- src/query.ts
+++ src/query.ts
@@ -1,10 +1,23 @@
 import { HLTV } from 'hltv'
 import { TEAM } from './constant'
 
+// TODO: adjust API according to schema
 
 const handleError = err => console.log(`${err.name} ${err.errno}`)
 
-export const getTeamMatch = (name: string) => HLTV.getTeam({ id: TEAM[name] }).then(res => console.log(res.recentResults), handleError)
+const getTeam = (name: string) => HLTV.getTeam({ id: TEAM[name] })
+
+export const getTeamMatch = (name: string) => getTeam(name).then(res => console.log(res.recentResults), handleError)
+
+export const getTeamOverview = (name: string) => getTeam(name).then(res => console.log(res.overview), handleError)
+
+export const getTeamRanking = (name: string) => {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = today.getMonth()
+  const day = today.getDate()
+  HLTV.getTeamRanking({ year, month, day }).then(console.log, handleError)
+}
 
 export const getTeamStats = (name: string) => HLTV.getTeamStats({ id: TEAM[name] }).then(console.log, handleError)
 
