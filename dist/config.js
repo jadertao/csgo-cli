a5eb1cc55d22857a2e0d16315e2f4945a6a61815
jader <jader_tao@qq.com>
2017-11-11 16:24:32 +0800

    update: adjust option priority

--- dist/config.js
+++ dist/config.js
@@ -1,14 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var modifyContent = function (payload) { return ({ type: 'content', payload: payload }); };
+var query_1 = require("./query");
 exports.config = {
     version: '0.1.0',
     command: [
         {
             name: 'team [name]',
             alias: 't',
-            description: '查询队伍信息',
-            option: ['--content,-c <type>', 'specify query content', modifyContent]
+            description: '查询队伍信息,每次只能查询一种',
+            option: [
+                ['-m --match', 'query recent matches according to team'],
+                ['-o --overview', 'query overview according to team'],
+                ['-r --ranking', 'query current ranking according to team']
+            ],
+            handler: function (name, options) {
+                console.log(name);
+                if (options.match && !options.overview && !options.ranking) {
+                    console.log("querying recent matches of " + name + ",pleaze wait");
+                    query_1.getTeamMatch(name);
+                }
+                if (!options.match && options.overview && !options.ranking) {
+                    console.log("querying team overview of " + name + ",pleaze wait");
+                    query_1.getTeamOverview(name);
+                }
+                if (!options.match && !options.overview && options.ranking) {
+                    console.log("querying current team ranking of " + name + ",pleaze wait");
+                    query_1.getTeamRanking(name);
+                }
+            }
         }
     ]
 };
--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,20 @@
 
 import { getTeamMatch, getTeamOverview, getTeamRanking } from './query'
 
+interface command {
+  name: string,
+  alias: string,
+  description: string,
+  option: Array<Array<string>>,
+  handler: any
+}
 
+interface config {
+  version: string,
+  command: Array<command>
+}
 
-
-export const config = {
+export const config: config = {
   version: '0.1.0',
   command: [
     {
@@ -12,16 +22,25 @@ export const config = {
       alias: 't',
       description: '查询队伍信息,每次只能查询一种',
       option: [
-        ['--match -m', 'query recent matches according to team'],
-        ['--overview -o', 'query overview according to team'],
-        ['--ranking -r', 'query current ranking according to team']
+        ['-m --match', 'query recent matches according to team'],
+        ['-o --overview', 'query overview according to team'],
+        ['-r --ranking', 'query current ranking according to team']
       ],
       handler: (name: string, options: any) => {
-        if (options.match && !options.overview && !options.ranking) getTeamMatch(name)
-        if (!options.match && options.overview && !options.ranking) getTeamOverview(name)
-        if (!options.match && !options.overview && options.ranking) getTeamRanking(name)
+        console.log(name)
+        if (options.match && !options.overview && !options.ranking) {
+          console.log(`querying recent matches of ${name},pleaze wait`)
+          getTeamMatch(name)
+        }
+        if (!options.match && options.overview && !options.ranking) {
+          console.log(`querying team overview of ${name},pleaze wait`)
+          getTeamOverview(name)
+        }
+        if (!options.match && !options.overview && options.ranking) {
+          console.log(`querying current team ranking of ${name},pleaze wait`)
+          getTeamRanking(name)
+        }
       }
     }
-  ],
-  handler: ''
+  ]
 }
