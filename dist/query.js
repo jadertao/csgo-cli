d47e198390130877ab1ea8209a622bf5af5c63e2
jader <jader_tao@qq.com>
2017-11-11 16:25:28 +0800

    adjust: small fix

--- dist/query.js
+++ dist/query.js
@@ -3,10 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var hltv_1 = require("hltv");
 var constant_1 = require("./constant");
 var handleError = function (err) { return console.log(err.name + " " + err.errno); };
-exports.getTeamMatch = function (name) { return hltv_1.HLTV.getTeam({ id: constant_1.TEAM[name] }).then(function (res) { return console.log(res.recentResults); }, handleError); };
-exports.getTeamStats = function (name) { return hltv_1.HLTV.getTeamStats({ id: constant_1.TEAM[name] }).then(console.log, handleError); };
-var classifyMap = {
-    match: exports.getTeamMatch,
-    stats: exports.getTeamStats
+var getTeam = function (name) { return hltv_1.HLTV.getTeam({ id: constant_1.TEAM[name] }); };
+var getTeamStats = function (name) { return hltv_1.HLTV.getTeamStats({ id: constant_1.TEAM[name] }); };
+exports.getTeamMatch = function (name) { return getTeam(name).then(function (res) { return console.log(res.recentResults); }, handleError); };
+exports.getTeamOverview = function (name) { return getTeamStats(name).then(function (res) { return console.log(res.overview); }, handleError); };
+exports.getTeamRanking = function (name) {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth();
+    var day = today.getDate();
+    hltv_1.HLTV.getTeamRanking({ year: "" + year, month: "" + month, day: "" + day }).then(console.log, handleError);
 };
-exports.classify = function (type) { return classifyMap[type]; };
--- src/query.ts
+++ src/query.ts
@@ -7,23 +7,18 @@ const handleError = err => console.log(`${err.name} ${err.errno}`)
 
 const getTeam = (name: string) => HLTV.getTeam({ id: TEAM[name] })
 
+const getTeamStats = (name: string) => HLTV.getTeamStats({ id: TEAM[name] })
+
 export const getTeamMatch = (name: string) => getTeam(name).then(res => console.log(res.recentResults), handleError)
 
-export const getTeamOverview = (name: string) => getTeam(name).then(res => console.log(res.overview), handleError)
+export const getTeamOverview = (name: string) => getTeamStats(name).then(res => console.log(res.overview), handleError)
 
 export const getTeamRanking = (name: string) => {
   const today = new Date()
   const year = today.getFullYear()
   const month = today.getMonth()
   const day = today.getDate()
-  HLTV.getTeamRanking({ year, month, day }).then(console.log, handleError)
+  HLTV.getTeamRanking({ year: `${year}`, month: `${month}`, day: `${day}` }).then(console.log, handleError)
 }
 
-export const getTeamStats = (name: string) => HLTV.getTeamStats({ id: TEAM[name] }).then(console.log, handleError)
 
-const classifyMap = {
-  match: getTeamMatch,
-  stats: getTeamStats
-}
-
-export const classify = (type: string) => classifyMap[type]
